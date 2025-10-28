45d0bb70e531fb45184df8f3b6bffbcf52a8197b
makgopajm <139314817+makgopajm@users.noreply.github.com>
2025-10-28 07:27:47 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         
-        System.out.println("-----***** Let's if you work now *****-----");
+        System.out.println("-----***** Testing Webhooks *****-----");
 
     }
 }
